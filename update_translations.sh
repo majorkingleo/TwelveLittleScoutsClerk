c47f23693ae32419d79a911488d9b3ba4df180c2
Martin Oberzalek <kingleo@gmx.at>
2026-05-12 21:38:58 +0200

    added first translation

---
+++ update_translations.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+pkg_to_filename() {
+    FILE=$(basename "$1")
+    echo "$FILE" | sed 's/^[a-z][a-z.]*\.\([A-Z].*\)/\1/'
+}
+
+for trans in ~/.TwelveLittleScoutsClerk/translations/*_de.properties; do
+    FILENAME=$(pkg_to_filename "$trans")
+    cp "$trans" src/at/redeye/twelvelittlescoutsclerk/resources/translations/"$FILENAME"
+done
